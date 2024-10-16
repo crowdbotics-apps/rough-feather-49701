5754b5a41f19be9e8553dc72638c536d72bf906a
Crowdbotics <team@crowdbotics.com>
2024-10-16 20:30:01 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rough-feather-49701",
+  url: "https://rough-feather-49701.botics.co",
+  api: "https://rough-feather-49701.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
